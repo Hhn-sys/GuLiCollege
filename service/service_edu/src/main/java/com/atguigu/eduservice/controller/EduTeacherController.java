1c90fe4099e9e4c5ac0447940e2e6c40bc4371d1
Hhn丶 <1282737853@qq.com>
2021-09-09 21:35:28 +0800

    EduTeacher代码的框架

---
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -0,0 +1,21 @@
+package com.atguigu.eduservice.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 讲师 前端控制器
+ * </p>
+ *
+ * @author codeGenerator
+ * @since 2021-09-05
+ */
+@RestController
+@RequestMapping("/eduservice/teacher")
+public class EduTeacherController {
+
+}
+
