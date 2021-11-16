4b2aa3fa5db610161ee3c4f65c56bdf64f5c8e77
Hhn丶 <1282737853@qq.com>
2021-11-16 20:08:46 +0800

    学习到P25的代码

--- service/service_edu/src/main/java/com/atguigu/eduservice/config/EduConfig.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/config/EduConfig.java
@@ -2,6 +2,7 @@ package com.atguigu.eduservice.config;
 
 import com.baomidou.mybatisplus.core.injector.ISqlInjector;
 import com.baomidou.mybatisplus.extension.injector.LogicSqlInjector;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +14,15 @@ public class EduConfig {
      * 逻辑删除插件配置
      */
     @Bean
-    public ISqlInjector sqlInjector(){
+    public ISqlInjector sqlInjector() {
         return new LogicSqlInjector();
+    }
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
     }
 }
--- service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/atguigu/eduservice/controller/EduTeacherController.java
@@ -1,12 +1,19 @@
 package com.atguigu.eduservice.controller;
 
 
+import com.atguigu.commonutils.R;
 import com.atguigu.eduservice.entity.EduTeacher;
+import com.atguigu.eduservice.entity.vo.TeacherQueryVO;
 import com.atguigu.eduservice.service.EduTeacherService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import io.swagger.models.auth.In;
+import org.apache.poi.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -34,8 +41,9 @@ public class EduTeacherController {
      */
     @GetMapping("findAll")
     @ApiOperation(value = "所有讲师列表")
-    public List<EduTeacher> findAllTeacher() {
-        return teacherService.list(null);
+    public R findAllTeacher() {
+        List<EduTeacher> list = teacherService.list(null);
+        return R.ok().data("items", list);
     }
 
     /**
@@ -47,11 +55,72 @@ public class EduTeacherController {
      * @RequestParam（单独的数据）
      */
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true)
-                                 @PathVariable String id) {
-        return teacherService.removeById(id);
+    @ApiOperation(value = "逻辑删除讲师")
+    public R removeTeacher(@ApiParam(name = "id", value = "讲师ID", required = true)
+                           @PathVariable String id) {
+        boolean flag = teacherService.removeById(id);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
     }
 
+    // 分页查询讲师
+    @ApiOperation(value = "分页查询讲师")
+    @GetMapping("pageTeacher/{current}/{limit}}")
+    public R pageListTeacher(@PathVariable long current, @PathVariable long limit) {
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        // 调用分页时会把底层数据存储在pageTeacher中直接返回。
+        teacherService.page(pageTeacher, null);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total", total).data("records", records);
+    }
+
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition(@PathVariable long current, @PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQueryVO teacherQueryVO) {
+        // 创建page对象
+        Page<EduTeacher> pageTeacher = new Page<>(current, limit);
+        // 构造条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        // 多条件组合查询
+        String name = teacherQueryVO.getName();
+        Integer level = teacherQueryVO.getLevel();
+        String begin = teacherQueryVO.getBegin();
+        String end = teacherQueryVO.getEnd();
+
+        // 判断条件是否为空，不空则拼接条件
+        if (!StringUtils.isEmpty(name)) {
+            wrapper.like("name", name);
+        }
+        if (!StringUtils.isEmpty(level)) {
+            wrapper.eq("level", level);
+        }
+        if (!StringUtils.isEmpty(begin)) {
+            wrapper.ge("gmt_create", begin);
+        }
+        if (!StringUtils.isEmpty(end)) {
+            wrapper.le("gmt_create", end);
+        }
+        teacherService.page(pageTeacher, wrapper);
+        long total = pageTeacher.getTotal();
+        List<EduTeacher> records = pageTeacher.getRecords();
+        return R.ok().data("total", total).data("records", records);
+
+    }
+
+    // 添加讲师接口的方法
+    @PostMapping("addTeacher")
+    public R addTeacher(@RequestBody EduTeacher eduTeacher) {
+        boolean flag = teacherService.save(eduTeacher);
+        if (flag) {
+            return R.ok();
+        } else {
+            return R.error();
+        }
+    }
 
 }
 
