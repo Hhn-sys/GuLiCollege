5b1c51e123251cf13a1f7ae9e044fdfb6efcb266
Hhn丶 <1282737853@qq.com>
2021-11-19 20:56:13 +0800

    第二天完成，即P33完成

--- common/service_base/src/main/java/com/atguigu/servicebase/GlobalExceptionHandler.java
+++ common/service_base/src/main/java/com/atguigu/servicebase/GlobalExceptionHandler.java
@@ -1,17 +1,21 @@
 package com.atguigu.servicebase;
 
 import com.atguigu.commonutils.R;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice
+@Slf4j // 加入日志文件需要用这个注解
 public class GlobalExceptionHandler {
 
     // 指定出现什么异常执行这个方法
     @ExceptionHandler(Exception.class)
     @ResponseBody // 为了返回数据
     public R error(Exception e){
+        // 将error加入日志文件中
+        log.error(e.getMessage());
         e.printStackTrace();
         return R.error().message("执行了全局异常捕获");
     }
